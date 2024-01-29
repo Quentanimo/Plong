f457c7fa86aed9f38c7358d6b333d9b3fc975c38
ZestFilly <zestfilly@proton.me>
2024-01-29 17:59:38 -0600

    Serving up BALLS
    Balls now are served to player that won previous round.
    
    Randomized gamestart serve and vector.y for serve as well

--- Plong/Assets/Scripts/KickoffBall.cs
+++ Plong/Assets/Scripts/KickoffBall.cs
@@ -6,19 +6,14 @@ public class KickoffBall : MonoBehaviour
 {
 
     public float speed;
+    public float ServeDelay = 1f; //Time Between ball being instantiated and it being served to players
     Vector2 direction; //(x,y)
     Rigidbody2D rb;
     private GameObject GameMaster = null;
-    public
+    private bool _P1WonLastRound = false;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        //kickoff
-        rb = GetComponent<Rigidbody2D>();
-        direction = Vector2.one.normalized;//(1,1)
-
-
         //find the game master object via the tag "GameController"
         //Name this game object as "GameMaster" for this script
         GameMaster = GameObject.FindGameObjectWithTag("GameController");
@@ -27,6 +22,20 @@ public class KickoffBall : MonoBehaviour
         {
             Debug.Log("Error: Ball was unable to find Game Master. Check that Tag has not changed");
         }
+
+        //check to see which player won last round in order to determine direction to serve ball
+        _P1WonLastRound = GameMaster.GetComponent<ScoreTracker>().P1WonLastRound;
+    }
+    
+     private void Start()
+    {
+        //kickoff
+        rb = GetComponent<Rigidbody2D>();
+
+        //delays ball being served to players by seconds as specified in ServeDelay
+        //calls WhoGetsBall() which determines which direction the ball should be served
+        Invoke("WhoGetsBall", ServeDelay);
+
     }
 
     // Update is called once per frame
@@ -61,4 +70,18 @@ public class KickoffBall : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    void WhoGetsBall()
+    {
+        //if P1 won last round serve ball to P1, if not then serve towards P2
+        if (_P1WonLastRound == true)
+        {
+            direction = new Vector2(-1, Random.value);//(1,1)
+        }
+        else
+        {
+            direction = new Vector2(1, Random.value);//(1,1)
+        }
+
+    }
 }
+
--- Plong/Assets/Scripts/ScoreTracker.cs
+++ Plong/Assets/Scripts/ScoreTracker.cs
@@ -9,14 +9,14 @@ public class ScoreTracker : MonoBehaviour
     [SerializeField] private int Player1Score = 0;
     [SerializeField] private int Player2Score = 0;
     public RoundReset _RoundReset;
-
+    public bool P1WonLastRound;     //did P1 win last round? used for determining direction ball should be served. Randomized at Start
     public Text Player1ScoreText;
     public Text Player2ScoreText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        P1WonLastRound = (Random.value > 0.5f);
     }
 
     // Update is called once per frame
@@ -27,14 +27,16 @@ public class ScoreTracker : MonoBehaviour
     public void GoalPlayer1()
     {
         Player1Score++;
-        Debug.Log("GOAL PLAYER 1!");
+        P1WonLastRound = true;
         Player1ScoreText.text = Player1Score.ToString();
         _RoundReset.Reset();
+        
     }
 
     public void GoalPlayer2()
     {
         Player2Score++;
+        P1WonLastRound = false;
         Debug.Log("GOAL PLAYER 2!");
         Player2ScoreText.text = Player2Score.ToString();
         _RoundReset.Reset();
