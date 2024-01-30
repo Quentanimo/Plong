f9c2bb90620eb62dbd29c4bfde163f637be01dea
ZestFilly <zestfilly@proton.me>
2024-01-30 17:22:30 -0600

    Sound Overhaul (basically the previous commit is trash)
    Found a way to handle sound that will make it easier to control via a settings menu later.
    
    Therefore did an entire overhaul of how the sound system works
    
    Add the Play On Hit Collision script to any object, give it a sound file, and you've a SFX on hit.
    
    Features include:
    -multiple sound effects played at random
    -customizable filter by tag functionality to filter what objects can trigger the SFX
    -fully adaptively coded. This script is ready to travel baby. Take it to pong or to Furry Fury: Beat (and fuck) Em' Up. It don't matter
    
    Possible concerns:
    I changed the tag on the ball from "Respawn" to "Ball", I feel like it's more intuitive. I don't know if the old tag was referenced anywhere, but the game still seems to run fine.
    
    Also apparently, "Great Commit Summaries contain less than 50 characters", well you know that's why the average commit summary is just 'great' and not perfect

--- Plong/Assets/Scripts/PlayClipOnCollision.cs
+++ Plong/Assets/Scripts/PlayClipOnCollision.cs
@@ -5,21 +5,85 @@ using UnityEngine;
 
 public class PlayClipOnCollision : MonoBehaviour
 {
+    /* ==========================================================================================================
+     * -------------------------------------Play Clip on Collision-----------------------------------------------
+     * 
+     * Script that cues the SFXplayer under AudioMaster parent object to play a clip on collision with the ball
+     * Script can be added into any game object with collider
+     * Script needs audio clips to be dragged and dropped into the Clip array
+     * Clipselect selects which clip should play on non-randomized play
+     * for random play to work at least 2 clips must be loaded into the clip array
+     * ==========================================================================================================
+     */
 
-   //plays a single audio clip whenever a collision with the ball occurs
+    //-----------------------------------------Declarations------------------------------------------------------
+    private GameObject _AudioMaster;   //Object that controls Audio settings
+    private SFXClips SFXPlayer;        //Script that exists on SFX child of AudioMaster
+    public AudioClip[] Clip;           //Array of Clips, add whatever audio clips you'd like to the array
 
-    private AudioSource m_Audio;
-    // Start is called before the first frame update
+    [SerializeField] private            //Add a tag so SFX is only triggered by collision with objects that share tag
+    string FilterByTag = null;          //leave null to allow any collision to trigger SFX
+    
+    [SerializeField] private            //for non-randomized plays, 
+    int ClipSelect = 0;                 //select which clip should play by its number in the array
+    
+    [SerializeField] private            //check this box to enable script to randomly select a clip from the array
+    bool RandomClip = false;            //to play each time a collision is triggered on this object
+    //-----------------------------------------------------------------------------------------------------------
+ 
+    //--------------------------------------------Start----------------------------------------------------------
     void Start()
     {
-        m_Audio = GetComponent<AudioSource>();
-    }
+        //find script SFXClips nested under audiomaster
+        //this script essentially handles actually playing the audio clips that we feed it
+        //Currently SFXClips has limited functionality but will have more use later
+        _AudioMaster = GameObject.FindWithTag("Audio");
+        SFXPlayer = _AudioMaster.transform.Find("SFX").GetComponent<SFXClips>();
 
+    }
+    //----------------------------------------------------------------------------------------------------------
+
+    //----------------------------------Collision Detection-----------------------------------------------------
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Respawn"))
+        //check to see if a tag has been given to filter by
+        if (FilterByTag != null)
         {
-            m_Audio.Play();
+            //check to see if collision is with the object specified by FilterByTag
+            if (collision.gameObject.CompareTag(FilterByTag))
+            {
+                PlaySFX(); //see function below
+            }
+        }
+        //no tag, no filter, play that baby everytime
+        else
+        {
+            PlaySFX(); //see function below
         }
     }
+    //---------------------------------------------------------------------------------------------------------
+
+    //---------------------------------------PlaySFX()---------------------------------------------------------
+    private void PlaySFX()
+    {
+        //check to make sure that the array has enough clips to be randomized
+        //and prevent Clip[1] being indexed with no element there
+        if (Clip.Length > 1 & RandomClip)
+        {
+            SFXPlayer.PlaySFX( Clip[ Random.Range( 0, Clip.Length ) ] );
+        }
+
+        // make sure the manually entered clip select number is compatible with the array
+        else if (ClipSelect <= Clip.Length)
+        {
+            SFXPlayer.PlaySFX(Clip[ClipSelect]);
+            //send the clip at the index specified to Clip
+        }
+
+        else
+        {
+            Debug.Log("SFX Error: Clip[] index is outside of range of the array. Please check" +gameObject.name);
+        }
+    }
+    //---------------------------------------------------------------------------------------------------------
 }
