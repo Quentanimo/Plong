d5e3d62520d8828b46d442fe3e8c358efa22c900
ZestFilly <zestfilly@proton.me>
2024-02-26 22:24:00 -0600

    Implemented Scriptable Objects for Audio
    Small, and albeit unnecessary change. Used ScriptableObjects to store large audio clip arrays. I had already done this the hard way so there's no real gain for this project, but the PlayClip script that works off of scriptables should be helpful for future projects
    
    essentially it's the same, but now the aduio clips are stored on a scriptableobject. This means say if you want to have  10 objects play from the same list of 5 random audio clips, instead of having to manually give each one those 5 clips for a total of performing this action 50 times. Now you you just give each the scriptableobject, reducing manual asignments to 10 actions instead.

---
+++ Plong/Assets/Scripts/Sound Scripts/AudioFilesSFX.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data",menuName ="ScriptableObjects/Audio", order =1)]
+public class AudioFilesSFX : ScriptableObject
+{
+
+    public AudioClip[] AudioFiles;
+    
+
+
+}
